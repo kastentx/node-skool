519ec3ec92e85e8130cfda9e2228780e8bf9c21c
Nick Kasten <kastencode@gmail.com>
2016-11-13 03:08:25 -0600

    get request working, displaying full output

--- lesson7.js
+++ lesson7.js
@@ -5,6 +5,10 @@ console.log(passedUrl)
 
 http.get(passedUrl, function(res) {
 	console.log("got response: " + res.statusCode)
+	res.setEncoding("utf8")
+	.on('data', function(chunk) {
+	console.log("body: " + chunk)
+	})
 }).on('error', function(e) {
 	console.log("got error: " + e.message)
 })
