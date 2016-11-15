129280ffecaae51065ed542e779018a46c9bc053
Nick Kasten <kastencode@gmail.com>
2016-11-14 22:23:27 -0600

    lesson 8 completed

--- lesson8.js
+++ lesson8.js
@@ -1,10 +1,16 @@
 var http = require('http')
 var passedUrl = process.argv[2]
 
+var allChars = ""
+
 http.get(passedUrl, function(res) {
 	res.setEncoding("utf8")
 	.on('data', function(chunk) {
-	console.log(chunk)
+		allChars += chunk
+	})
+	.on('end', function() {
+		console.log(allChars.toString().length)
+		console.log(allChars)
 	})
 }).on('error', function(e) {
 	console.log("got error: " + e.message)
